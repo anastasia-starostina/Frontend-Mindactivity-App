eb9a76ccf39681c54c1d115c07890defaeb6b891
edgarasmarkunas <markunas11@gmail.com>
2022-06-21 12:30:51 +0100

    background component is working!

---
+++ mindactivity/src/Components/App/App.js
@@ -0,0 +1,19 @@
+
+import Particle from '../ParticleBackground/Particle';
+import './App.css';
+
+function App() {
+  return (
+    <>
+     <Particle/> 
+     <div>
+      <h1 className='App'>Mindactivity</h1>
+      
+    </div>
+    
+      
+    </>
+  );
+}
+
+export default App;
