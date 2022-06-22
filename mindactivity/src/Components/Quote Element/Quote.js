61d4c78fab65e6eaa9616eb3e19e5eeb378202b7
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-06-22 12:47:14 +0100

    quotes fetched correctly

--- mindactivity/src/Components/Quote Element/Quote.js
+++ mindactivity/src/Components/Quote Element/Quote.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
 //fetch request to get a quote to render in the quote component
 
-const Quote = () => {
-    return (
-      <div class="blockquote-wrapper">
-        <div class="blockquote">
-          <h1>Start before you are ready</h1>
-        </div>
+const Quote = ({ quote }) => {
+  return (
+    <div class="blockquote-wrapper">
+      <div class="blockquote">
+        <h1>{quote}</h1>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Quote;
