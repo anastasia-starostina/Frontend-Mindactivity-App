4b87d35c407881acc7a60e46c33dcdf80f7e86f2
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-06-22 16:41:34 +0100

    some comment added again

--- mindactivity/src/Components/Button/index.js
+++ mindactivity/src/Components/Button/index.js
@@ -13,35 +13,5 @@ function PlayButton() {
   );
 }
 
-function PauseButton() {
-  const sound = new Howl({
-    src: ["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3"],
-    html5: true,
-  });
-  return (
-    <div>
-      <button onClick={() => sound.pause()}>music off</button>
-    </div>
-  );
-}
-
-// const SoundButton = () => {
-//     const soundSrc = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3";
-
-//     const callMySound = (src) => {
-//         const sound = new Howl({
-//             src,
-//             html5: true,
-//         });
-//         sound.play()
-//     }
-
-//     return (
-//         <>
-//         <button onClick = {() => callMySound(soundSrc)}>Click me to play sound</button>
-//         </>
-
-//     )
-// }
-
+//test comment
 export default PlayButton;
