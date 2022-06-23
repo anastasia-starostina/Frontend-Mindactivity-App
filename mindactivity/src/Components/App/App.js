fd73a65fadd05b3b15962e49a48f13cd536013b4
edgarasmarkunas <markunas11@gmail.com>
2022-06-23 15:30:50 +0100

    setting up a fetch request for blogs

--- mindactivity/src/Components/App/App.js
+++ mindactivity/src/Components/App/App.js
@@ -4,25 +4,45 @@ import React from "react";
 import MyTimer from "../Timer";
 import Quote from "../Quote Element";
 import Blogpost from "../Blogpost/index";
+
 function App() {
   const time = new Date();
   time.setSeconds(time.getSeconds() + 300);
   const [affirmations, setAffirmations] = React.useState([
     { content: undefined },
   ]);
+  const [blogs, setBlogs] = React.useState([])
+
   React.useEffect(() => {
     fetchAffirmations();
+    fetchBlogs();
   }, []);
+
+
   const fetchAffirmations = async () => {
     const response = await fetch("http://localhost:3001/mindactivity");
     const data = await response.json();
     // console.log(`this is data: ${data}`);
     setAffirmations([...data.payload]);
   };
+
+  const fetchBlogs = async () => {
+    const response = await fetch("http://localhost:3001/mindactivity/blogs");
+    const data = await response.json();
+    setBlogs([...data.payload]);
+  }
+
   const randomIndex = Math.floor(Math.random() * 50);
   console.log(randomIndex);
+
+  const randomBlogIndex = Math.floor(Math.random() * 3);
+  console.log(randomBlogIndex)
+
   let firstItem = { ...affirmations[randomIndex] };
   // console.log(firstItem.content);
+
+  let firstBlog = { ...blogs[randomBlogIndex]}
+
   return (
     <>
       <Particle />
@@ -39,4 +59,5 @@ function App() {
     </>
   );
 }
+
 export default App;
